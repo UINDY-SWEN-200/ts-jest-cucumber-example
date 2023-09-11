fcd28273d69719fdd9c1b21de2acb84f11e612c7
Steve Spicklemire <steve@spvi.com>
2023-09-11 11:46:09 +0000

    add prettier

--- library.ts
+++ library.ts
@@ -1,54 +1,54 @@
 export interface IBook {
-  name: string
-  author: string
-  checkedOut: boolean
+    name: string
+    author: string
+    checkedOut: boolean
 }
 
 export interface ILibrary {
-  addBook({ name, author }: Partial<IBook>): void
-  checkoutBook(name: string): void
-  findBook(name: string): IBook | undefined
-  isCheckedOut(name: string): boolean
+    addBook({ name, author }: Partial<IBook>): void
+    checkoutBook(name: string): void
+    findBook(name: string): IBook | undefined
+    isCheckedOut(name: string): boolean
 }
 
 export class Library implements ILibrary {
-  private books: IBook[] = []
-  public static NO_SUCH_BOOK = "No Such Book"
-  public static BOOK_CHECKED_OUT = "Book Checked Out"
-    
-  addBook({ name, author }: Partial<IBook>): void {
-    if (name && author) {
-      this.books.push({ name, author, checkedOut: false })
-    }
-  }
+    private books: IBook[] = []
+    public static NO_SUCH_BOOK = 'No Such Book'
+    public static BOOK_CHECKED_OUT = 'Book Checked Out'
 
-  findBook(name: string): IBook | undefined {
-    return this.books.find((book) => book.name === name)
-  }
-
-  checkoutBook(name: string): void {
-    // Check out a book with the name "name".
-    // if the book doesn't exist, throw an exception
-    const aBook: IBook | undefined = this.findBook(name)
-    if (aBook) {
-      if (aBook.checkedOut) {
-        throw(new Error(Library.BOOK_CHECKED_OUT))
-      } else {
-        aBook.checkedOut = true
-      }
-    } else {
-      throw(new Error(Library.NO_SUCH_BOOK))
+    addBook({ name, author }: Partial<IBook>): void {
+        if (name && author) {
+            this.books.push({ name, author, checkedOut: false })
+        }
     }
-  }
 
-  isCheckedOut(name: string): boolean {
-    let result = false
-    const aBook: IBook | undefined = this.findBook(name)
-    if (aBook) {
-      result = aBook.checkedOut
-    } else {
-      throw(new Error(Library.NO_SUCH_BOOK))
+    findBook(name: string): IBook | undefined {
+        return this.books.find((book) => book.name === name)
     }
-    return result
-  }
+
+    checkoutBook(name: string): void {
+        // Check out a book with the name "name".
+        // if the book doesn't exist, throw an exception
+        const aBook: IBook | undefined = this.findBook(name)
+        if (aBook) {
+            if (aBook.checkedOut) {
+                throw new Error(Library.BOOK_CHECKED_OUT)
+            } else {
+                aBook.checkedOut = true
+            }
+        } else {
+            throw new Error(Library.NO_SUCH_BOOK)
+        }
+    }
+
+    isCheckedOut(name: string): boolean {
+        let result = false
+        const aBook: IBook | undefined = this.findBook(name)
+        if (aBook) {
+            result = aBook.checkedOut
+        } else {
+            throw new Error(Library.NO_SUCH_BOOK)
+        }
+        return result
+    }
 }
--- steps/library.steps.ts
+++ steps/library.steps.ts
@@ -1,53 +1,57 @@
-import { defineFeature, loadFeature } from "jest-cucumber"
-import { ILibrary, IBook, Library } from "../library"
+import { defineFeature, loadFeature } from 'jest-cucumber'
+import { ILibrary, IBook, Library } from '../library'
 
-const feature = loadFeature("./features/library.feature")
+const feature = loadFeature('./features/library.feature')
 
 defineFeature(feature, (test) => {
-  let library: ILibrary
+    let library: ILibrary
 
-  beforeEach(() => {
-    library = new Library()
-  })
+    beforeEach(() => {
+        library = new Library()
+    })
 
-  test("Check out a book.", ({ given, when, then }) => {
-    let checkedOutBookName: string // keep in test scope
-    given(
-      "A user wishes to check out a book from a library with these books:",
-      (table) => {
-        ;(table as any[]).forEach((row) => {
-          library.addBook({ name: row.Name, author: row.Author })
+    test('Check out a book.', ({ given, when, then }) => {
+        let checkedOutBookName: string // keep in test scope
+        given(
+            'A user wishes to check out a book from a library with these books:',
+            (table) => {
+                ;(table as any[]).forEach((row) => {
+                    library.addBook({ name: row.Name, author: row.Author })
+                })
+            }
+        )
+
+        when('They check out a book with a specific name:', (table) => {
+            checkedOutBookName = (table as any[])[0].Name
+            library.checkoutBook(checkedOutBookName)
         })
-      }
-    )
 
-    when("They check out a book with a specific name:", (table) => {
-      checkedOutBookName = (table as any[])[0].Name
-      library.checkoutBook(checkedOutBookName)
+        then('It should be marked as checked out.', () => {
+            expect(library.isCheckedOut(checkedOutBookName)).toBe(true)
+        })
     })
 
-    then("It should be marked as checked out.", () => {
-      expect(library.isCheckedOut(checkedOutBookName)).toBe(true)
+    test("Try to check out a book that isn't present", ({
+        given,
+        when,
+        then,
+    }) => {
+        let myBook = ''
+
+        given('a book you want is not in the library', () => {
+            library.addBook({ name: 'another book', author: 'somebody' })
+        })
+
+        when('you attempt to check it out', () => {
+            myBook = 'my book'
+        })
+
+        then('it throws an error', () => {
+            const attempt = () => {
+                library.checkoutBook(myBook)
+            }
+
+            expect(attempt).toThrow()
+        })
     })
-  })
-
-  test('Try to check out a book that isn\'t present', ({ given, when, then }) => {
-    let myBook = ""
-
-    given('a book you want is not in the library', () => {
-      library.addBook({name:"another book", author:"somebody"})
-    });
-
-    when('you attempt to check it out', () => {
-      myBook = "my book"
-    });
-
-    then('it throws an error', () => {
-      const attempt = () => {
-        library.checkoutBook(myBook)
-      }
-
-      expect(attempt).toThrow()
-    });
-  });
 })
